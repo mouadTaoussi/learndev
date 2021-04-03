c8c60888ee41562d51d302e6aa60fcb26de6d22c
mouadtaoussi <mouadtaoussi0@gmail.com>
2021-04-03 20:31:10 +0100

    set sameSite attribute to none

--- moon/index.js
+++ moon/index.js
@@ -46,7 +46,7 @@ app.use(express_session_1.default({
         port: main_config_1.default.redis_port,
         client: redisClient
     }),
-    cookie: { secure: true, sameSite: "none" }
+    cookie: { secure: true, }
 }));
 app.use(passport_1.default.initialize());
 app.use(passport_1.default.session());
