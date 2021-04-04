bf63f9c684ad9e0ea90be139c6540287af8fabfe
mouadtaoussi <mouadtaoussi0@gmail.com>
2021-04-04 12:24:07 +0100

    set sameSite attribute to none

--- venus/index.ts
+++ venus/index.ts
@@ -55,7 +55,7 @@ app.use(express_session({
 		port: main_config.redis_port, 
 		client: redisClient 
 	}),
-	cookie: { secure: true, sameSite : "none" } // Error here !!!
+	// cookie: { secure: true, sameSite : "none" } // Error here !!!
 }))
 
 // Ïnit passport app and routes
