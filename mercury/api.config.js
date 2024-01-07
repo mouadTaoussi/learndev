1b1a136fedaff6b8385ad7aa7cffc99afbfe2e8a
mouadTaoussi <mouadtaoussi0@gmail.com>
2024-01-07 15:28:40 +0100

    Migrate backend host from heroku

--- mercury/api.config.js
+++ mercury/api.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-	api_domain : "https://learndevelopment.herokuapp.com" // <-- host of the backend that the app use to make requests
+	// api_domain : "https://learndevelopment.herokuapp.com" // <-- host of the backend that the app use to make requests
 	// api_domain : "http://localhost:8000" // <-- host of the backend that the app use to make requests
+	api_domain : "https://learndev.onrender.com/" // <-- host of the backend that the app use to make requests
 }
 
 // https://learndev-backend.herokuapp.com/graphql
